37f5607ce7fc11495e2377a59471ee8a91577e5d
ggaspar22 <andrijchornobajj@gmail.com>
2021-12-06 10:07:42 +0200

    added hw 7

---
+++ script.js
@@ -0,0 +1,45 @@
+//1 zavdannia
+
+// person = {
+//     firstName: 'Ivan',
+//     secondName:'Ivanov',
+//     showData(){
+//         console.log('Name:',this.firstName,'Surname:',this.secondName);
+//     }
+// }
+
+// newPerson = {};
+//     for(const key in person){
+//         newPerson[key] = person[key];
+//     }
+//     newPerson.firstName = 'Petro';
+//     newPerson.secondName = 'Petriv';
+//     person.showData();
+//     newPerson.showData();
+
+// 2 zavdannia
+
+// MyMath = {
+//     a: 5,
+//     b: 2,
+//     sum(){
+//         let c = this.a + this.b;
+//         console.log('a + b = ',c);
+//     },
+//     multiplication(){
+//         let d = this.a*this.b;
+//         console.log('a * b = ',d);
+//     },
+//     division(){
+//         let e = this.a/this.b;
+//         console.log('a / b = ',e);
+//     },
+//     subtraction(){
+//         let f = this.a - this.b;
+//         console.log('a - b = ',f);
+//     }
+// }
+// MyMath.sum();
+// MyMath.multiplication();
+// MyMath.division();
+// MyMath.subtraction();
